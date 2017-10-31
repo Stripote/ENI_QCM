6a9e7981140a3fef02e69f788e491b796f7a4f94
avilleret2017 <avilleret2017@12-UC29-22.ad.campus-eni.fr>
2017-10-31 16:13:13 +0100

    connection de l'utilisateur et creation de la session

---
+++ src/fr/eni/qcm/servlet/deconnecter.java
@@ -0,0 +1,45 @@
+package fr.eni.qcm.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Deconnecter
+ */
+public class deconnecter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public deconnecter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		deconnection(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		deconnection(request, response);
+	}
+	
+	protected void deconnection(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (session!=null)
+			session.invalidate();
+		response.sendRedirect(request.getContextPath()+"/");
+	}
+
+}
