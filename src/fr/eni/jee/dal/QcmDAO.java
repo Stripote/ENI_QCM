35cb917c050189d22062bfed50d9386adb63459a
ahingant2017 <ahingant2017@12-UC29-11.ad.campus-eni.fr>
2017-11-02 11:36:40 +0100

    Bibi11

--- src/fr/eni/jee/dal/QcmDAO.java
+++ src/fr/eni/jee/dal/QcmDAO.java
@@ -1,12 +1,11 @@
 package fr.eni.jee.dal;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
-
 import fr.eni.jee.bo.Qcm;
 import fr.eni.qcm.util.AccesBase;
 
--- src/fr/eni/qcm/servlet/listerQcm.java
+++ src/fr/eni/qcm/servlet/listerQcm.java
@@ -1,5 +1,6 @@
 package fr.eni.qcm.servlet;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 
