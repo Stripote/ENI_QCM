6afd565b52b16ed1b03d3752667e101ce71fa9c3
ahingant2017 <ahingant2017@12-UC29-11.ad.campus-eni.fr>
2017-11-02 10:05:16 +0100

    Bibi8

--- src/fr/eni/qcm/servlet/listerQcm.java
+++ src/fr/eni/qcm/servlet/listerQcm.java
@@ -1,10 +1,17 @@
 package fr.eni.qcm.servlet;
 
+import java.awt.List;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.jee.bo.Qcm;
+import fr.eni.jee.dal.QcmDAO;
 
 /**
  * Servlet implementation class listerQcm
@@ -24,16 +31,32 @@ public class listerQcm extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		processRequest(request, response);
 	}
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try{
+			
+			
+			ArrayList<Qcm> qcms = QcmDAO.rechercher();
+			
+			request.setAttribute("listeQcm", qcms);
+			
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/menuCandidat");
+			dispatcher.forward(request,response);
+							
+			
+		}catch (Exception e){
+			
+		}
+		
+	}
 }
