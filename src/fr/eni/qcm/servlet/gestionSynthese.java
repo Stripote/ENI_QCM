dc49ec661a9fd39a5a9c4c72924bf5d71e075fb5
avilleret2017 <avilleret2017@12-UC29-22.ad.campus-eni.fr>
2017-11-03 12:09:39 +0100

    mise à jour gestionTest et gestionSynthese

--- src/fr/eni/qcm/servlet/gestionSynthese.java
+++ src/fr/eni/qcm/servlet/gestionSynthese.java
@@ -1,10 +1,16 @@
 package fr.eni.qcm.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.jee.bo.Qcm;
+import fr.eni.jee.bo.Question;
 
 /**
  * Servlet implementation class gestionSynthese
@@ -24,16 +30,29 @@ public class gestionSynthese extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		gestionnaireSynthese(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		gestionnaireSynthese(request, response);
+	}
+	
+	protected void gestionnaireSynthese(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Qcm qcm =(Qcm)request.getAttribute("qcm");
+		List<Question> liste  =new ArrayList<Question>();
+		
+		
+		for (int indexSection = 0; indexSection < (qcm.getSections().size()-1); indexSection++) {
+			for (int indexQues = 0; indexQues < (qcm.getSections().get(indexSection).getLesQuestions().size()-1); indexQues++) {
+				Question question= qcm.getSections().get(indexSection).getLesQuestions().get(indexQues);
+				liste.add(question);
+			}
+		}
+		
+		
 	}
 
 }
